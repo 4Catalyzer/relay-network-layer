c78cc867c58e9286330927e0f34df9ad243db5c7
Jason Quense <monastic.panic@gmail.com>
2020-01-09 18:18:27 -0500

    Add readme

--- src/NetworkLayer.ts
+++ src/NetworkLayer.ts
@@ -8,15 +8,55 @@ interface NetworkLayer {
 }
 
 export interface NetworkLayerOptions {
-  path?: string;
+  /** The graphql API endpoint, provide a pathname or fully qualified url. */
   url?: string;
+
+  /**
+   * The socket.io endpoint, provide a pathname or fully qualified url.
+   *
+   * **MUST** be provided in order to enable subscription support
+   */
   subscriptionUrl?: string;
+
+  /**
+   * Batches requests within a time frame into a single request for more
+   * efficient fetching. requests are sent as a JSON array. Mutatations and file uploads
+   * are NOT batched.
+   *
+   * Defaults to `true`.
+   *
+   * **Requires a Graphql server that understands batching"
+   */
   batch?: boolean;
+
+  /** An authorization token sent in a header with every request */
   token?: string;
+
+  /** The prefix string in the auth header defaults to "Bearer " */
   authPrefix?: string;
+
+  /** The header the `token` is sent in defaults to "Authorization" */
   authHeader?: string;
+
+  /**
+   * Any fetch API "init" details, this is based directly to the `fetch` call, see
+   * [MDN](https://developer.mozilla.org/en-US/docs/Web/API/Request/Request) for API details.
+   */
   init?: RequestInit;
+
+  /**
+   * The amount of time to wait before a batch is closed and sent to the server.
+   *
+   * The default is `0ms`, or about the next tick of the event loop.
+   */
   batchTimeout?: number;
+
+  /**
+   * The max number of concurrent subscriptions allowed.
+   * After the number has been no more will be set to the server (a console.warn is issued informing you the limit has been reached)
+   *
+   * The default is: `200`
+   */
   maxSubscriptions?: number;
 }
 
--- src/createSubscribe.ts
+++ src/createSubscribe.ts
@@ -1,6 +1,22 @@
 import { Observable, RequestParameters, Variables } from 'relay-runtime';
 import io from 'socket.io-client';
 
+enum _APOLLO_TYPES {
+  GQL_CONNECTION_INIT = 'connection_init', // Client -> Server
+  GQL_CONNECTION_ACK = 'connection_ack', // Server -> Client
+  GQL_CONNECTION_ERROR = 'connection_error', // Server -> Client
+
+  // NOTE: The keep alive message type does not follow the standard due to connection optimizations
+  GQL_CONNECTION_KEEP_ALIVE = 'ka', // Server -> Client
+
+  GQL_CONNECTION_TERMINATE = 'connection_terminate', // Client -> Server
+  GQL_START = 'start', // Client -> Server
+  GQL_DATA = 'data', // Server -> Client
+  GQL_ERROR = 'error', // Server -> Client
+  GQL_COMPLETE = 'complete', // Server -> Client
+  GQL_STOP = 'stop', // Client -> Server
+}
+
 export interface SubscriptionOptions {
   url?: string;
   token?: string;
